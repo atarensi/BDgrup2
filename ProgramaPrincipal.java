a214a3c45a06b45f998da2c9f6d337b8dbe5bf2f
MarioFlores44 <123104623+MarioFlores44@users.noreply.github.com>
2023-02-07 12:19:11 +0100

    Add files via upload

---
+++ ProgramaPrincipal.java
@@ -0,0 +1,9 @@
+public class ProgramaPrincipal {
+    public static void main(String[] args) {
+        ImportarComunitatsAutonomes();
+    }
+
+    public static void ImportarComunitatsAutonomes() {
+        Leer.comunitatsAutonomes();
+    }
+}
