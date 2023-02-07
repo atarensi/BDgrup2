5f7a41f2b549970f2ed6e6eca86a99da78c6359a
MarioFlores44 <123104623+MarioFlores44@users.noreply.github.com>
2023-02-07 13:52:30 +0100

    Update para el Insert de Provincies

--- src/ProgramaPrincipal.java
+++ src/ProgramaPrincipal.java
@@ -1,9 +1,24 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ProgramaPrincipal {
-    public static void main(String[] args) {
+    public static Connection con;
+    public static void main(String[] args) throws SQLException {
+        con= DriverManager.getConnection("jdbc:mysql://10.2.112.28:3306/prova","perepi","pastanaga");
         ImportarComunitatsAutonomes();
+        ImportProvincies();
+        con.close();
     }
 
+    // Importación de los datos de la tabla Comunitats Autonomes
     public static void ImportarComunitatsAutonomes() {
-        Leer.comunitatsAutonomes();
+        Leer.comunitatsAutonomes(con);
+    }
+
+
+    // Importación de los datos de la tabla Provincies
+    public static void ImportProvincies() {
+        Leer.provincies(con);
     }
 }
