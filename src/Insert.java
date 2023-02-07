1cc50d4c988e3722cc55153531602d7b075e7a3c
MarioFlores44 <123104623+MarioFlores44@users.noreply.github.com>
2023-02-07 13:51:16 +0100

    Update del Insert de Provincies

--- src/Insert.java
+++ src/Insert.java
@@ -3,22 +3,20 @@ import java.util.Calendar;
 public class Insert {
 
     // Función que coge la información de las comunidades autónomas y lo inserta en la BBDD
-    public static void comunitatsAutonomes(int id, String nom, String ine){
+    public static void comunitatsAutonomes(int id, String nom, String ine, Connection con){
 
         try {
 
             Class.forName("com.mysql.cj.jdbc.Driver");
 
-            Connection con= DriverManager.getConnection("jdbc:mysql://10.2.112.28:3306/prova","perepi","pastanaga");
-
             Calendar calendar = Calendar.getInstance();
             java.sql.Date startDate = new java.sql.Date(calendar.getTime().getTime());
-            String query = " INSERT INTO comunitats_autonomes (comunitat_aut_id,nom,codi_ine)"
+            String query = " INSERT INTO comunitats_autonomes (comunitat_aut_id, nom,codi_ine)"
                     + " values (?, ?, ?)";
 
             // create the mysql insert preparedstatement
             PreparedStatement preparedStmt = con.prepareStatement(query);
-            preparedStmt.setInt    (1, id);
+            preparedStmt.setInt(1, id);
             preparedStmt.setString (2, nom);
             preparedStmt.setString (3, ine);
 
@@ -27,11 +25,39 @@ public class Insert {
             // execute the preparedstatement
             preparedStmt.execute();
 
-            //Tanquem la connexió
-            con.close();
         }catch(Exception e){
             System.out.println(e);
         }
     }
 
+
+    // Función que coge la información de las provincias y la inserta en la BBDD
+    public static void provincies (int id, String nom, String ine, int num_escons, Connection con) {
+
+        try {
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            Calendar calendar = Calendar.getInstance();
+            java.sql.Date startDate = new java.sql.Date(calendar.getTime().getTime());
+            String query = " INSERT INTO provincies (comunitat_aut_id,nom,codi_ine,num_escons)"
+                    + " values (?, ?, ?, ?)";
+
+            // create the mysql insert preparedstatement
+            PreparedStatement preparedStmt = con.prepareStatement(query);
+            preparedStmt.setInt(1, id);
+            preparedStmt.setString (2, nom);
+            preparedStmt.setString (3, ine);
+            preparedStmt.setInt(4,num_escons);
+
+
+
+            // execute the preparedstatement
+            preparedStmt.execute();
+
+        }catch(Exception e){
+            System.out.println(e);
+        }
+
+    }
 }
