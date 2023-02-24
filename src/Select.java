53babfb4483587feb72d48fd04ac301840e43aa5
MarioFlores44 <123104623+MarioFlores44@users.noreply.github.com>
2023-02-24 13:12:35 +0100

    Update Select.java

--- src/Select.java
+++ src/Select.java
@@ -2,6 +2,8 @@ import java.sql.*;
 import java.util.Calendar;
 
 public class Select {
+
+    // Método para hacer un Select e insertar la id de comunitats autonomes según el código ine
     public static int provincies(Connection con, String ca) {
         int x = 0;
         try {
@@ -18,6 +20,32 @@ public class Select {
             ResultSet rs = preparedStmt.executeQuery();
             if (rs.next()) {
                 x = rs.getInt("comunitat_aut_id");
+            }
+        }
+        catch(Exception e){
+            System.out.println(e);}
+
+        return x;
+    }
+
+    // Método para haver un Select e insertr la id de provincias según el código ine
+
+    public static int municipis(Connection con, String ine_prov) {
+        int x = 0;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            //SENTÈNCIA SELECT
+            // Busca ID de comunitat autonoma para introducirlo en la tabla Provincias
+            String query = "SELECT provincia_id " +
+                    " FROM provincies " +
+                    "WHERE codi_ine = ?";
+            PreparedStatement preparedStmt = con.prepareStatement(query);
+
+            preparedStmt.setString(1, ine_prov);
+
+            ResultSet rs = preparedStmt.executeQuery();
+            if (rs.next()) {
+                x = rs.getInt("provincia_id");
             }
         }
         catch(Exception e){
