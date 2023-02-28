312cecda2cec5f037be43d6a5815b7cef74e1425
adrian2bueno <113520848+adrian2bueno@users.noreply.github.com>
2023-02-28 13:57:06 +0100

    Update BD_eleccions_v2.sql

--- BD/BD_eleccions_v2.sql
+++ BD/BD_eleccions_v2.sql
@@ -1,1 +1,6 @@
-Alter
+-- ???????????
+-- Alterem la taula eleccions_municipis per que es generin les columnes
+ALTER TABLE eleccions_municipis
+MODIFY COLUMN vots_emesos INT UNSIGNED GENERATED ALWAYS AS (vots_valids + vots_blanc + vots_nuls) STORED,
+MODIFY COLUMN vots_valids INT UNSIGNED GENERATED ALWAYS AS (vots_canditadures) STORED,
+MODIFY COLUMN vots_candidatures	INT UNSIGNED GENERATED ALWAYS AS (vots_valids + vots_blanc) STORED;
