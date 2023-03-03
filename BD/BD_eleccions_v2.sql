5b0966b1742c9d90dbe022e000130fa7bea33283
Alejandro Vázquez Carrión <123106711+ElTinii@users.noreply.github.com>
2023-03-03 10:09:04 +0100

    Update BD_eleccions_v2.sql

--- BD/BD_eleccions_v2.sql
+++ BD/BD_eleccions_v2.sql
@@ -1,6 +1,149 @@
--- ???????????
--- Alterem la taula eleccions_municipis per que es generin les columnes
-ALTER TABLE eleccions_municipis
-MODIFY COLUMN vots_emesos INT UNSIGNED GENERATED ALWAYS AS (vots_valids + vots_blanc + vots_nuls) STORED,
-MODIFY COLUMN vots_valids INT UNSIGNED GENERATED ALWAYS AS (vots_canditadures) STORED,
-MODIFY COLUMN vots_candidatures	INT UNSIGNED GENERATED ALWAYS AS (vots_valids + vots_blanc) STORED;
+CREATE TABLE comunitats_autonomes (
+	comunitat_aut_id TINYINT UNSIGNED AUTO_INCREMENT,
+	nom				VARCHAR(45),
+	codi_ine		CHAR(2) NOT NULL,
+	CONSTRAINT pk_comunitats_autonomes PRIMARY KEY (comunitat_aut_id),
+	CONSTRAINT uk_com_aut_codi_ine UNIQUE (codi_ine)    
+);
+CREATE TABLE provincies (
+	provincia_id		TINYINT UNSIGNED AUTO_INCREMENT,
+	comunitat_aut_id	TINYINT UNSIGNED,
+	nom				VARCHAR(45),
+	codi_ine		CHAR(2),
+	num_escons		TINYINT UNSIGNED,
+	CONSTRAINT pk_provincies PRIMARY KEY (provincia_id),
+		CONSTRAINT  fk_provincies_comunitats_autonomes FOREIGN KEY (comunitat_aut_id) 
+			REFERENCES comunitats_autonomes (comunitat_aut_id),
+	CONSTRAINT uk_com_aut_codi_ine UNIQUE (codi_ine)
+);
+CREATE TABLE municipis (
+	municipi_id		SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+	nom				VARCHAR(100),
+	codi_ine		CHAR(3) NOT NULL,
+	provincia_id	TINYINT UNSIGNED NOT NULL ,
+	districte		CHAR(2),
+	CONSTRAINT pk_municipis PRIMARY KEY (municipi_id),
+		CONSTRAINT fk_municipis_provincies FOREIGN KEY (provincia_id) 
+			REFERENCES provincies (provincia_id),
+	CONSTRAINT uk_municipis_codi_ine UNIQUE (codi_ine, provincia_id,districte) *
+);
+CREATE TABLE eleccions (
+	eleccio_id	TINYINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    nom			VARCHAR(45),
+    data		DATE  NOT NULL,
+    any			YEAR  NOT NULL,
+    mes			TINYINT UNSIGNED NOT NULL,
+    CONSTRAINT pk_eleccions PRIMARY KEY (eleccio_id),
+	CONSTRAINT uk_eleccions_any_mes UNIQUE (any,mes), 
+    CONSTRAINT uk_eleccions_data UNIQUE (data)
+);
+
+CREATE TABLE eleccions_municipis (
+	eleccio_id		TINYINT UNSIGNED NOT NULL,
+    municipi_id		SMALLINT UNSIGNED NOT NULL,
+    num_meses		SMALLINT UNSIGNED,
+    cens			INT UNSIGNED,
+    vots_emesos		INT UNSIGNED,
+    vots_valids		INT UNSIGNED,
+    vots_candidatures	INT UNSIGNED,
+    vots_blanc		INT UNSIGNED,
+    vots_nuls		INT UNSIGNED,
+    CONSTRAINT pk_eleccions_municipis PRIMARY KEY (eleccio_id,municipi_id,num_meses,cens),
+		CONSTRAINT fk_eleccions_municipis_municipis FOREIGN KEY (municipi_id) 
+			REFERENCES municipis (municipi_id),
+        CONSTRAINT fk_eleccions_municipis_eleccions FOREIGN KEY (eleccio_id) 
+			REFERENCES eleccions (eleccio_id)		
+);
+
+CREATE TABLE candidatures (
+	candidatura_id		INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    eleccio_id			TINYINT UNSIGNED NOT NULL,
+    codi_candidatura	CHAR(6),
+    nom_curt			VARCHAR(50),
+    nom_llarg			VARCHAR(150),
+    codi_acumulacio_provincia	CHAR(6),
+    codi_acumulacio_ca			CHAR(6),
+    codi_acumulario_nacional	CHAR(6),
+    CONSTRAINT pk_eleccions PRIMARY KEY (candidatura_id),
+		CONSTRAINT fk_eleccions_partits_eleccions FOREIGN KEY (eleccio_id) 
+			REFERENCES eleccions (eleccio_id),
+	CONSTRAINT uk_eleccions_partits UNIQUE (eleccio_id,codi_candidatura)
+);
+
+CREATE TABLE persones (
+	persona_id		INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    nom				VARCHAR(30) ,
+    cog1			VARCHAR(30),
+    cog2			VARCHAR(30),
+    sexe			ENUM('M','F'),
+    data_naixement	DATE,
+    dni				CHAR(10),
+    CONSTRAINT pk_persones PRIMARY KEY (persona_id),
+	CONSTRAINT uk_candidats_dni UNIQUE (dni)
+);
+
+CREATE TABLE candidats (
+	candidat_id		BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    candidatura_id	INT UNSIGNED NOT NULL,
+    persona_id		INT UNSIGNED,
+    provincia_id	TINYINT UNSIGNED,
+    num_ordre		TINYINT,
+    tipus			ENUM('T','S'),
+    CONSTRAINT pk_candidats PRIMARY KEY (candidat_id),
+		CONSTRAINT fk_candidats_provincies1 FOREIGN KEY (provincia_id) 
+			REFERENCES provincies (provincia_id),
+		CONSTRAINT fk_candidats_persones1 FOREIGN KEY (persona_id) 
+			REFERENCES persones (persona_id),
+		CONSTRAINT fk_candidats_candidatures1 FOREIGN KEY (candidatura_id) 
+			REFERENCES candidatures (candidatura_id),
+	CONSTRAINT uk_candidats_persona_cand UNIQUE (candidatura_id,persona_id)
+);
+
+CREATE TABLE vots_candidatures_ca (
+	comunitat_autonoma_id	TINYINT UNSIGNED NOT NULL,
+	candidatura_id		INT UNSIGNED NOT NULL,
+	vots			INT UNSIGNED,
+	CONSTRAINT pk_vots_candidatures_ca PRIMARY KEY (comunitat_autonoma_id,candidatura_id),
+	CONSTRAINT fk_comunitats_autonomes_has_candidatures_comunitats_autonomes1 FOREIGN KEY (comunitat_autonoma_id) 
+		REFERENCES comunitats_autonomes (comunitat_aut_id),
+	CONSTRAINT fk_comunitats_autonomes_has_candidatures_candidatures1 FOREIGN KEY (candidatura_id) 
+		REFERENCES candidatures (candidatura_id)
+);
+CREATE TABLE vots_candidatures_prov (
+	provincia_id		TINYINT UNSIGNED NOT NULL,
+	candidatura_id		INT UNSIGNED NOT NULL,
+	vots			INT UNSIGNED COMMENT "Número de vots obtinguts per la candidatura",
+	candidats_obtinguts	SMALLINT UNSIGNED COMMENT "Número de candidats obtinguts per la candidatura",
+	CONSTRAINT pk_vots_candidatures_prov PRIMARY KEY (provincia_id,candidatura_id),
+		CONSTRAINT fk_candidatures_provincies_provincies1 FOREIGN KEY (provincia_id) 
+			REFERENCES provincies (provincia_id),
+		CONSTRAINT fk_candidatures_provincies_candidatures1 FOREIGN KEY (candidatura_id) 
+			REFERENCES candidatures (candidatura_id)
+);
+
+CREATE TABLE vots_candidatures_mun (
+	eleccio_id		TINYINT UNSIGNED NOT NULL,
+	municipi_id		SMALLINT UNSIGNED NOT NULL,
+	candidatura_id		INT UNSIGNED NOT NULL,
+	vots			INT UNSIGNED COMMENT "Número de vots obtinguts per la candidatura",
+	CONSTRAINT pk_vots_candidatures_mun PRIMARY KEY (eleccio_id,municipi_id,candidatura_id),
+		CONSTRAINT fk_candidatures_municipis_candidatures1 FOREIGN KEY (candidatura_id) 
+			REFERENCES candidatures (candidatura_id),
+		CONSTRAINT fk_candidatures_municipis_eleccions_municipis1 FOREIGN KEY (eleccio_id,municipi_id) 
+			REFERENCES eleccions_municipis (eleccio_id,municipi_id)
+);
+
+
+INSERT INTO eleccions VALUES('1','Eleccions generals espanya 2020','2019-11-24','2019','11');
+
+DROP TABLE vots_candidatures_prov;
+DROP TABLE vots_candidatures_mun;
+DROP TABLE vots_candidatures_ca;
+DROP TABLE candidats;
+DROP TABLE persones;
+DROP TABLE eleccions_municipis;
+DROP TABLE candidatures;
+DROP TABLE municipis;
+DROP TABLE provincies;
+DROP TABLE eleccions;
+DROP TABLE comunitats_autonomes;
