4f58c597ba6877500b35d45de16f573d7f3cd9de
Alejandro Vázquez Carrión <123106711+ElTinii@users.noreply.github.com>
2023-03-03 12:39:11 +0100

    Update ProgramaPrincipal.java

--- src/ProgramaPrincipal.java
+++ src/ProgramaPrincipal.java
@@ -4,54 +4,75 @@ import java.sql.SQLException;
 
 public class ProgramaPrincipal {
     public static Connection con;
-    public static void main(String[] args) throws SQLException {
-        con= DriverManager.getConnection("jdbc:mysql://10.2.112.28:3306/prova","perepi","pastanaga");
-        ImportarComunitatsAutonomes();
-        ImportProvincies();
-        ImportMunicipis();
-        ImportEleccionesMunicipales();
-        ImportCandidaturas();
-        ImportPersonas();
-        ImportVosotsMunicipales();
-        ImportVotsProvincial();
 
+    public static void main(String[] args) throws SQLException {
+        con = DriverManager.getConnection("jdbc:mysql://192.168.56.103/eleccions_generals_espanyoles", "perepi", "pastanaga");
+        ImportarComunitatsAutonomes();//@todo be
+        System.out.println("Comunitats Autonomes importades");
+        ImportProvincies();//@todo be
+        System.out.println("Provincies importades");
+        ImportMunicipis(); //@todo be
+        System.out.println("Municipis importats");
+        ImportEleccionesMunicipales(); //@todo malament
+        System.out.println("Elecciones municipales importades");
+        ImportarCandidatures(); //@todo be
+        System.out.println("Candidatures importades");
+        ImportPersonas(); //@todo be
+        System.out.println("Personas importades");
+        ImportarCandidats(); //@todo be
+        System.out.println("Candidats importats");
+        ImportVotosMunicipales(); //@todo malament
+        System.out.println("Votos municipales importats");
+        ImportVotosCA(); //@todo malament
+        System.out.println("Votos CA importats");
+        ImportVotsProvincial(); //@todo be
+        System.out.println("Vots provincial importats");
         con.close();
     }
 
     // Importación de los datos de la tabla Comunitats Autonomes
     public static void ImportarComunitatsAutonomes() {
+
         Leer.comunitatsAutonomes(con);
     }
-
-
     // Importación de los datos de la tabla Provincies
     public static void ImportProvincies() {
+
         Leer.provincies(con);
     }
-
-    // Importación de los datos de la tabla Municipis
+    // Importación de los datos de lal tabla Municipis
     public static void ImportMunicipis() {
+
         Leer.municipis(con);
     }
-    
-     // Importacion de los datos de la tabla Eleccions Municipals
+    // Importacion de los datos de la tabla Eleccions Municipals
     public static void ImportEleccionesMunicipales() {
+
         Leer.elecciones_municipales(con);
     }
+    public static void ImportarCandidatures(){
 
-    // Importación de los datos de la tabla Candidaturas
-    public static void ImportCandidaturas() {
-        Leer.municipis(con);
+        Leer.candidatures(con);
     }
-    
-        // Importación de los datos de la tabla Personas
+    // Importación de los datos de la tabla Personas
     public static void ImportPersonas() {
+
         Leer.personas(con);
     }
-    
+    public static void ImportarCandidats(){
+
+        Leer.candidats(con);
+    }
     // Importación de los datos de la tabla Vots_candidatures_Municipals
     public static void ImportVotosMunicipales() {
+
         Leer.votos_municipales(con);
+    }
+
+    // Importación de los datos de la tabla Vots_candidatures_ca
+    public static void ImportVotosCA() {
+
+        Leer.votos_ca(con);
     }
     //Importación de los datos de la tabla Vots_candidatures_Provincies
     public static void ImportVotsProvincial(){
