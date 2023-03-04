af30bd5c409e6bba4d1134bc7f5f742bd6900772
Alejandro Vázquez Carrión <123106711+ElTinii@users.noreply.github.com>
2023-03-04 14:29:56 +0100

    Update BD_eleccions_v2.sql

--- BD/BD_eleccions_v2.sql
+++ BD/BD_eleccions_v2.sql
@@ -3,19 +3,13 @@ ALTER TABLE provincies
 	MODIFY COLUMN comunitat_aut_id	TINYINT UNSIGNED,
 	MODIFY COLUMN codi_ine		CHAR(2);
 
-
 ALTER TABLE municipis 
-	MODIFY COLUMN codi_ine		CHAR(5),
-	ADD CONSTRAINT uk_municipis_provincia_id UNIQUE (municipi_id,provincia_id,codi_ine);
+	MODIFY COLUMN codi_ine		CHAR(7);
 
-
-ALTER TABLE eleccions_municipis 
-    	DROP PRIMARY KEY,
-	ADD CONSTRAINT pk_eleccions_municipis PRIMARY KEY (eleccio_id,municipi_id,num_meses,cens),
-    	DROP CONSTRAINT uk_eleccions_municipis;
         
 ALTER TABLE persones
 MODIFY COLUMN dni CHAR(10);
 
 INSERT INTO eleccions (eleccio_id, nom, data)
 	VALUES(1,'Eleccions generals espanya 2020','2019-11-24');
+
